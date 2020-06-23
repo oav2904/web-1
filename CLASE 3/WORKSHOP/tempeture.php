72cbb0bad7ae113227948f16862dd4086ac9f6af
oscar <oariasv@est.utn.ac.cr>
2020-06-23 01:32:48 -0500

    workshop3

---
+++ CLASE 3/WORKSHOP/tempeture.php
@@ -0,0 +1,21 @@
+<?php
+
+$tempeture = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 
+73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+
+$average = array_sum($tempeture) / count($tempeture);
+sort($tempeture);
+$short = array_unique(array_slice($tempeture, 0, 9));
+rsort($tempeture);
+$large = array_unique(array_slice($tempeture, 0, 7));
+
+echo $average ."\n";
+
+foreach($short as $value){
+echo  $value ."\n";
+
+}
+foreach($large as $value){
+echo  $value ."\n";
+
+}
